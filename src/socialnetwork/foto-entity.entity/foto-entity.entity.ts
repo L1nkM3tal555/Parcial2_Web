a79f56dccf1b3460406c0477d6fc37c5da7ae82f
Jesús Alejandro Dávila <69691593+L1nkM3tal555@users.noreply.github.com>
2023-11-30 10:37:29 -0500

    Entities

--- src/socialnetwork/foto-entity.entity/foto-entity.entity.ts
+++ src/socialnetwork/foto-entity.entity/foto-entity.entity.ts
@@ -1,1 +1,26 @@
-export class FotoEntityEntity {}
+
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { RedSocialEntityEntity } from "../red-social-entity.entity/red-social-entity.entity";
+
+
+@Entity()
+export class FotoEntityEntity {
+    @PrimaryGeneratedColumn("uuid")
+    id: long;
+ 
+    @Column()
+    fecha: string;
+ 
+    @Column()
+    iso: int;
+
+    @Column()
+    velObturacion: int;
+
+    @Column()
+    apertura: int;
+
+    @ManyToOne(() => RedSocialEntityEntity, redSocial => redSocial.fotos)
+    redSocial: RedSocialEntityEntity;
+
+}
